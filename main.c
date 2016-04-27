6952367757dcbd8f50066bfbf3422fe39a7152c9
AuburnBOSS <stolbennikov@bk.ru>
2016-04-27 09:14:33 +0600

    Реализована функция вычисления дискриминанта discriminant

--- main.c
+++ main.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
 #include <locale.h>
+#include <math.h>
+
+double discriminant(double a, double b, double c)
+{
+	double d;
+	d = pow(b, 2) - 4 * a * c;
+	return d;
+}
 
 int main()
 {
-	setlocale((LC_ALL, "Russian");
+	setlocale(LC_ALL, "Russian");
 	double a, b, c, x1, x2;
 	printf("Введите аргументы: a, b, c: ");
 	scanf("%lf %lf %lf", &a, &b, &c);
+	printf("Дискриминант = %4.0f\n", discriminant(a, b, c));
 	return 0;
 }
